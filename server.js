bb5845c3918bfc78cf13ff5b5f107f560051fd64
palanisamee <31619748+palanisamee@users.noreply.github.com>
2017-09-07 15:10:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article_one', function (req, res) {
+app.get('/art-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'art-one.html'));
 });
 
-app.get('/article_two', function (req, res) {
+app.get('/art-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'art-two.html'));
 });
 
-app.get('/article_three', function (req, res) {
+app.get('/art-three', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'art-three.html'));
 });
 
