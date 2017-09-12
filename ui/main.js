a6be0a7d7f3b2ebff56b3dac855475e56a6a1a24
palanisamee <31619748+palanisamee@users.noreply.github.com>
2017-09-12 13:09:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var button=doucument.getElementById('counter');
+button.onclick=function(0{
+    counter=counter+1;
+    var span= document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
+    
+});
