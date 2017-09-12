9ba0dd35d70b13438c685f0b4cba2f408256f8e9
palanisamee <31619748+palanisamee@users.noreply.github.com>
2017-09-12 13:40:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,15 +10,16 @@ button.onclick=function(){
         if (request.readystate ===XMLHttprequest.DONE){
             if (request.status===200){
                 var counter=request.responseText;
-                var span=document.getElemnetById("count");
-                span.innerHTML=counter.toString();
+                var span= document.getElementById('count');
+                 span.innerHTML=counter.toString();
                 
             }
         }
     }
-    counter=counter + 1;
-    var span= document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
+   
+    request.open('GET', 'http://mpalanisamee.imad.hasura-app.io/counter',true);
+    request.send(null);
+   
+     
     
 };
