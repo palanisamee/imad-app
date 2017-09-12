dcfaa57cc39ce56d25e3a11fd74fb063d47a2a1b
palanisamee <31619748+palanisamee@users.noreply.github.com>
2017-09-12 13:50:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ button.onclick= function(){
     
     //caputure
     request.onreadystatechange=function(){
-        if (request.readystate ===XMLHttprequest.DONE){
+        if (request.readystate ===XMLHttpRequest.DONE){
             if (request.status===200){
                 var counter   = request.responseText;
                 var span= document.getElementById('count');
