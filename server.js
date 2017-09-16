8f3799ec34317db67820ae9ead439579f95cac1a
palanisamee <31619748+palanisamee@users.noreply.github.com>
2017-09-16 11:45:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,6 +30,18 @@ function hash(input, salt) {
 return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 	} 
 	
+	var pool = new Pool(config);
+	app.get('/test-db ', function(req,res) {
+	pool.query('SELECT * FROM test', function (err, result){
+	    if (err) {
+	        res.status(500).send(err.toString());
+	    }else{
+	        res.send(JSON.stringify(result));
+	    }
+	    
+	});
+	   
+	});
 app.get('/hash/:input', function(req,res){
 	var hashedString =hash(req.params.input,'this-is-same-random-string');
 	res.send(hashedString);
